50f0b4d5476e160f6049419828ab59cc528bb064
Aban Ali <128579145+abaan016@users.noreply.github.com>
2024-03-21 13:07:25 -0700

    Delete 2nd-Sem-Eproject-Aptech directory

--- 2nd-Sem-Eproject-Aptech/ct&v/admin/db.php
+++
@@ -1,9 +0,0 @@
-<?php                    
-
-$con=mysqli_connect("localhost","root","","loginsystem");
-if(!$con)
-{
-    echo "<script>alert('database is not connected');</script>";
-}
-
-?>
--- 2nd-Sem-Eproject-Aptech/ct&v/admin/otherdb.php
+++
@@ -1,9 +0,0 @@
-<?php                    
-
-$conn=mysqli_connect("localhost","root","","ct&v");
-if(!$conn)
-{
-    echo "<script>alert('database is not connected');</script>";
-}
-
-?>
